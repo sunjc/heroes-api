0fb09a61dfabf376da8cdbf3f7c22163af99e560
sunjc <sunjc@iata.org>
2018-08-02 13:26:46 +0800

    init

--- src/main/java/org/itrunner/heroes/domain/Hero.java
+++ src/main/java/org/itrunner/heroes/domain/Hero.java
@@ -9,7 +9,7 @@ public class Hero {
     @Id
     @Column(name = "ID")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "HERO_SEQ")
-    @SequenceGenerator(name = "HERO_SEQ", sequenceName = "HERO_SEQ")
+    @SequenceGenerator(name = "HERO_SEQ", sequenceName = "HERO_SEQ", allocationSize = 1)
     private Long id;
 
     @Column(name = "NAME", unique = true, length = 30)
