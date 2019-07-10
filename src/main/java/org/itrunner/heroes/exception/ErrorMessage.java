5d8c13c34b8b1e089ff8ba387fc5edf562774991
sunjc <sunjc@iata.org>
2019-07-10 09:15:08 +0800

    refactor

--- src/main/java/org/itrunner/heroes/exception/ErrorMessage.java
+++ src/main/java/org/itrunner/heroes/exception/ErrorMessage.java
@@ -1,24 +1,24 @@
 package org.itrunner.heroes.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
 import lombok.Getter;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @ApiModel
 @Getter
-@RequiredArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ErrorMessage {
-    @NonNull
     private String type;
-
-    @NonNull
     private String message;
-
     private List<String> details = new ArrayList<>();
+
+    public ErrorMessage(String type, String message) {
+        this.type = type;
+        this.message = message;
+    }
 
     public void addDetailMessage(String message) {
         this.details.add(message);
