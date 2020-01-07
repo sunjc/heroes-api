42d524040e579f3ae86204e2814ad9847d23b23d
sunjc <sunjc@iata.org>
2020-01-07 12:28:22 +0800

    add swagger config

--- src/main/java/org/itrunner/heroes/config/SwaggerConfig.java
+++ src/main/java/org/itrunner/heroes/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package org.itrunner.heroes.config;
 
 import com.fasterxml.classmate.TypeResolver;
 import org.itrunner.heroes.exception.ErrorMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
@@ -23,11 +24,18 @@ import static com.google.common.collect.Lists.newArrayList;
 @Configuration
 public class SwaggerConfig {
 
+    private final SwaggerProperties properties;
+
+    @Autowired
+    public SwaggerConfig(SwaggerProperties properties) {
+        this.properties = properties;
+    }
+
     @Bean
     public Docket petApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("org.itrunner.heroes.controller"))
+                .apis(RequestHandlerSelectors.basePackage(properties.getBasePackage()))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo())
@@ -43,10 +51,10 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Api Documentation")
-                .description("Api Documentation")
-                .contact(new Contact("Jason", "http://blog.51cto.com/7308310", "sjc-925@163.com"))
-                .version("1.0")
+                .title(properties.getTitle())
+                .description(properties.getDescription())
+                .contact(new Contact(properties.getContact().getName(), properties.getContact().getUrl(), properties.getContact().getEmail()))
+                .version(properties.getVersion())
                 .build();
     }
 
@@ -57,11 +65,11 @@ public class SwaggerConfig {
     private SecurityContext securityContext() {
         return SecurityContext.builder()
                 .securityReferences(defaultAuth())
-                .forPaths(PathSelectors.regex("/api/.*"))
+                .forPaths(PathSelectors.regex(properties.getApiPath()))
                 .build();
     }
 
-   private List<SecurityReference> defaultAuth() {
+    private List<SecurityReference> defaultAuth() {
         AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = authorizationScope;
