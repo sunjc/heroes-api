07fa0d97d70050228d36039c134a1622af14b305
sunjc <sunjc@iata.org>
2020-03-06 07:01:48 +0800

    add @Modifying method

--- src/main/java/org/itrunner/heroes/repository/UserRepository.java
+++ src/main/java/org/itrunner/heroes/repository/UserRepository.java
@@ -2,9 +2,15 @@ package org.itrunner.heroes.repository;
 
 import org.itrunner.heroes.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
+    @Modifying
+    @Query("update User u set u.username = ?1 where u.email = ?2")
+    int setUsername(String username, String email);
 }
